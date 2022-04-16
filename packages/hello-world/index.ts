cd283c3c67282c86bd5e8fc100fe430c5d623104
fuhao1px <fuhao1px@163.com>
2022-04-16 10:55:18 +0800

    发布组件

--- packages/hello-world/index.ts
+++ packages/hello-world/index.ts
@@ -1,9 +1,9 @@
-import { withInstall } from '@lerna1/utils'
+import { withInstall } from '@butter-fly/utils'
 // import { withInstall } from '../utils/lib/index'
 
 import HelloWorld from './src/index.vue'
 
-export const FhHelloWorld = withInstall(HelloWorld)
-export default FhHelloWorld
+export const BFHelloWorld = withInstall(HelloWorld)
+export default BFHelloWorld
 
 export * from './src/index'
